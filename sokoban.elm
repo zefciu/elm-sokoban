9d47ee3b21609eb01435a95e626d0892b358cc3c
Szymon Pyżalski <szymon@pythonista.net>
2019-06-29 19:26:57 +0200

    Player moving but no pushing

--- sokoban.elm
+++ sokoban.elm
@@ -2,8 +2,12 @@ module Main exposing (main)
 
 import Array
 import Browser
+import Grid
 import Html
+import Keyboard
+import Keyboard.Arrows
 import List exposing (range)
+import Maybe
 
 
 type GameObject
@@ -13,12 +17,33 @@ type GameObject
     | Target
 
 
+isNonBlockingObject : GameObject -> Bool
+isNonBlockingObject o =
+    case o of
+        Target ->
+            True
+
+        _ ->
+            False
+
+
 type alias Field =
     List GameObject
 
 
+type alias Board =
+    Grid.Grid Field
+
+
 type alias Model =
-    Array.Array (Array.Array Field)
+    { pressedKeys : List Keyboard.Key
+    , playerPos : ( Int, Int )
+    , board : Board
+    }
+
+
+type Msg
+    = KeyMsg Keyboard.Msg
 
 
 viewField : Field -> Html.Html msg
@@ -41,6 +66,9 @@ viewField field =
                 [ Crate, Target ] ->
                     "C"
 
+                [] ->
+                    "."
+
                 _ ->
                     "!"
         ]
@@ -53,16 +81,125 @@ viewRow row =
 
 view : Model -> Html.Html msg
 view model =
-    Html.table [] <| Array.toList <| Array.map viewRow model
+    Grid.rows model.board
+        |> Array.map viewRow
+        |> Array.toList
+        |> Html.table []
 
 
-init =
-    Array.fromList
-        [ Array.fromList [ [ Wall ], [], [] ]
-        , Array.fromList [ [ Player ], [ Crate ], [ Target ] ]
-        , Array.fromList [ [], [], [] ]
-        ]
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.map KeyMsg Keyboard.subscriptions
+
+
+init : {} -> ( Model, Cmd msg )
+init _ =
+    ( { pressedKeys = []
+      , playerPos = ( 0, 1 )
+      , board =
+            Grid.fromList
+                [ [ [ Wall ], [], [] ]
+                , [ [ Player ], [ Crate ], [ Target ] ]
+                , [ [], [], [] ]
+                ]
+                |> Maybe.withDefault (Grid.repeat 0 0 [])
+      }
+    , Cmd.none
+    )
+
+
+processKey model keyMsg =
+    { model | pressedKeys = Keyboard.update keyMsg model.pressedKeys }
+
+
+getField : ( Int, Int ) -> Model -> Field
+getField ( x, y ) model =
+    Grid.get ( x, y ) model.board
+        |> Maybe.withDefault [ Wall ]
+
+
+isFree : Model -> ( Int, Int ) -> Bool
+isFree model ( x, y ) =
+    getField ( x, y ) model
+        |> List.all isNonBlockingObject
+
+
+canMove : Model -> Keyboard.Arrows.Arrows -> Bool
+canMove model arrows =
+    addArrows model.playerPos arrows
+        |> isFree model
+
+
+addArrows : ( Int, Int ) -> Keyboard.Arrows.Arrows -> ( Int, Int )
+addArrows ( x, y ) arrows =
+    ( x + arrows.x, y - arrows.y )
+
+
+removeObject : ( Int, Int ) -> GameObject -> Board -> Board
+removeObject ( x, y ) o board =
+    let
+        value =
+            Grid.get ( x, y ) board
+                |> Maybe.map (List.filter ((/=) o))
+                |> Maybe.withDefault []
+    in
+    Grid.set ( x, y ) value board
+
+
+putObject : ( Int, Int ) -> GameObject -> Board -> Board
+putObject ( x, y ) o board =
+    let
+        value =
+            Grid.get ( x, y ) board
+                |> Maybe.map ((::) o)
+                |> Maybe.withDefault []
+    in
+    Grid.set ( x, y ) value board
+
+
+moveObject : ( Int, Int ) -> ( Int, Int ) -> GameObject -> Board -> Board
+moveObject ( x1, y1 ) ( x2, y2 ) o board =
+    removeObject ( x1, y1 ) o board
+        |> putObject ( x2, y2 ) o
+
+
+move : Model -> Keyboard.Arrows.Arrows -> Model
+move model arrows =
+    let
+        destination =
+            addArrows model.playerPos arrows
+    in
+    { model
+        | board = moveObject model.playerPos destination Player model.board
+        , playerPos = destination
+    }
+
+
+processMove : Model -> ( Model, Cmd msg )
+processMove model =
+    let
+        arrows =
+            Keyboard.Arrows.arrows model.pressedKeys
+    in
+    if canMove model arrows then
+        ( move model arrows, Cmd.none )
+
+    else
+        ( model, Cmd.none )
+
+
+update : Msg -> Model -> ( Model, Cmd msg )
+update msg model =
+    case msg of
+        KeyMsg keyMsg ->
+            processKey model keyMsg
+                |> processMove
 
 
 main =
-    Browser.sandbox { init = init, view = view, update = identity }
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
